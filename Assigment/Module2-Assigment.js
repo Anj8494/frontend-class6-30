4f5fec21aaf676dcaf9d2de1996d60cff7f58fa4
Anj8494 <sharmaanjali3449@gmail.com>
2025-09-07 00:31:27 +0530

    add quiz senario

--- Assigment/Module2-Assigment.js
+++ Assigment/Module2-Assigment.js
@@ -23,41 +23,61 @@
 
 
 //Question 2
-const rose = 8;
-const lily = 10
-const tulip = 2
+// const rose = 8;
+// const lily = 10
+// const tulip = 2
 
-let roseQuantity = 70;
-let lilyQuantity = 50
-let tulipQuantity = 120;
+// let roseQuantity = 70;
+// let lilyQuantity = 50
+// let tulipQuantity = 120;
 
-let roseValue = rose * roseQuantity
-console.log(roseValue)
+// let roseValue = rose * roseQuantity
+// console.log(roseValue)
 
-let lilyValue = lily * lilyQuantity
-console.log(lilyValue)
+// let lilyValue = lily * lilyQuantity
+// console.log(lilyValue)
 
-let tulipValue = tulip * tulipQuantity
-console.log(tulipValue)
+// let tulipValue = tulip * tulipQuantity
+// console.log(tulipValue)
 
-totalValue = roseValue + lilyValue + tulipValue
-console.log(totalValue)
+// totalValue = roseValue + lilyValue + tulipValue
+// console.log(totalValue)
 
-roseQuantity1 = roseQuantity - 20;
-console.log(roseQuantity1)
-lilyQuantity1 = lilyQuantity - 30;
-console.log(lilyQuantity1)
+// roseQuantity1 = roseQuantity - 20;
+// console.log(roseQuantity1)
+// lilyQuantity1 = lilyQuantity - 30;
+// console.log(lilyQuantity1)
 
-roseValue1 = rose * roseQuantity1
-console.log(roseValue1)
+// roseValue1 = rose * roseQuantity1
+// console.log(roseValue1)
 
-lilyVAlue1 = lily * lilyQuantity1
-console.log(lilyQuantity1)
+// lilyVAlue1 = lily * lilyQuantity1
+// console.log(lilyQuantity1)
 
-total = roseValue1 + lilyVAlue1 + tulipValue
-console.log(total)
+// total = roseValue1 + lilyVAlue1 + tulipValue
+// console.log(total)
 
-console.log("rose unit Prize :", rose , ",Quantity :", roseQuantity1 ,",value: ", roseValue1)
-console.log("rose unit Prize :", lily , ",Quantity :", lilyQuantity1 ,",value: ", lilyVAlue1)
-console.log("rose unit Prize :", tulip , ",Quantity :", tulipQuantity ,",value: ", tulipValue)
-console.log("total value :", total)
+// console.log("rose unit Prize :", rose , ",Quantity :", roseQuantity1 ,",value: ", roseValue1)
+// console.log("rose unit Prize :", lily , ",Quantity :", lilyQuantity1 ,",value: ", lilyVAlue1)
+// console.log("rose unit Prize :", tulip , ",Quantity :", tulipQuantity ,",value: ", tulipValue)
+// console.log("total value :", total)
+
+//Module 2 Quiz: Scenario
+let name1 = "Maxwell Wright";
+let phone1 = "01917196495";
+let email1 ="  Curabitur.egestas.nunc@nonummyac.co.uk"
+
+let name2 = "Raja Villarreal";
+let phone2 = " 0866 398 2895 ";
+let email2 =" posuere.vulputate@sed.com"
+
+let name3 = "Helen Richards";
+let phone3 = "0800 1111 ";
+let email3 ="  libero@convallis.edu"
+
+console.log(name1, +"/"+ phone1 + "/" +email1)
+console.log(name3, +"/"+ phone3 + "/" +email3)
+
+
+
+
