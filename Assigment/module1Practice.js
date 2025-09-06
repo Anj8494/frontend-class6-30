31b2b8e2aeb838bad147360ac99705d16e9b9c9c
Anj8494 <sharmaanjali3449@gmail.com>
2025-09-06 23:14:26 +0530

    done module 1 practice

---
+++ Assigment/module1Practice.js
@@ -0,0 +1,24 @@
+//output your full name
+console.log("Anjali Sharmanode")
+
+// year of birth.
+console.log("1999")
+
+//passing the date without the quotes
+console.log(1999)
+
+//("abc", "def", "ghi");
+console.log("abc" , "def", "ghi")
+
+//Name Surname (Year) e.g. Mary Stuart (1542).
+console.log("Anjali", "Sharma" , "1999")
+//one argument;
+console.log("Anjali Sharma (1999)")
+
+//name, surname, and year as separate arguments.
+console.log("Anjali")
+console.log("Sharma")
+console.log("1999")
+
+//the + sign, for example "abc" + "def"
+console.log("Anjali" + " " + "Sharma" + " " + "1999")
