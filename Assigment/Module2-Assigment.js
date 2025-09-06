c686579d88ee86d27ae1f0b4a41c21ac39019de6
Anj8494 <sharmaanjali3449@gmail.com>
2025-09-06 23:41:10 +0530

    add question1 module2

---
+++ Assigment/Module2-Assigment.js
@@ -0,0 +1,22 @@
+//Question 1
+let rose = 8;
+let roseNumber= 70
+
+let lily= 10;
+let lilyNumber = 50 ;
+
+let tulip = 2
+let tulipNumber = 120;
+
+rosevalue= rose * roseNumber
+console.log(rosevalue)
+lilyValue = lily * lilyNumber
+console.log(lilyValue)
+
+tuliValue= tulip * tulipNumber
+console.log(tuliValue)
+
+
+console.log("Rose unit prize :",rose ,", quantity:",roseNumber, ",value:",rosevalue)
+console.log("lily unit prize :",lily ,", quantity:",lilyNumber, ",value:",lilyValue)
+console.log("tulip unit prize :",tulip ,", quantity:",tulipNumber, ",value:",tuliValue)
