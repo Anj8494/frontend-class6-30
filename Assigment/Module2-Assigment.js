caa1c804753fb591efaccb36fb1a2815f38a0934
Anj8494 <sharmaanjali3449@gmail.com>
2025-09-07 00:17:46 +0530

    add question 2 module 2

--- Assigment/Module2-Assigment.js
+++ Assigment/Module2-Assigment.js
@@ -1,22 +1,63 @@
 //Question 1
-let rose = 8;
-let roseNumber= 70
+// let rose = 8;
+// let roseNumber= 70
 
-let lily= 10;
-let lilyNumber = 50 ;
+// let lily= 10;
+// let lilyNumber = 50 ;
 
-let tulip = 2
-let tulipNumber = 120;
+// let tulip = 2
+// let tulipNumber = 120;
 
-rosevalue= rose * roseNumber
-console.log(rosevalue)
-lilyValue = lily * lilyNumber
+// rosevalue= rose * roseNumber
+// console.log(rosevalue)
+// lilyValue = lily * lilyNumber
+// console.log(lilyValue)
+
+// tuliValue= tulip * tulipNumber
+// console.log(tuliValue)
+
+
+// console.log("Rose unit prize :",rose ,", quantity:",roseNumber, ",value:",rosevalue)
+// console.log("lily unit prize :",lily ,", quantity:",lilyNumber, ",value:",lilyValue)
+// console.log("tulip unit prize :",tulip ,", quantity:",tulipNumber, ",value:",tuliValue)
+
+
+//Question 2
+const rose = 8;
+const lily = 10
+const tulip = 2
+
+let roseQuantity = 70;
+let lilyQuantity = 50
+let tulipQuantity = 120;
+
+let roseValue = rose * roseQuantity
+console.log(roseValue)
+
+let lilyValue = lily * lilyQuantity
 console.log(lilyValue)
 
-tuliValue= tulip * tulipNumber
-console.log(tuliValue)
+let tulipValue = tulip * tulipQuantity
+console.log(tulipValue)
 
+totalValue = roseValue + lilyValue + tulipValue
+console.log(totalValue)
 
-console.log("Rose unit prize :",rose ,", quantity:",roseNumber, ",value:",rosevalue)
-console.log("lily unit prize :",lily ,", quantity:",lilyNumber, ",value:",lilyValue)
-console.log("tulip unit prize :",tulip ,", quantity:",tulipNumber, ",value:",tuliValue)
+roseQuantity1 = roseQuantity - 20;
+console.log(roseQuantity1)
+lilyQuantity1 = lilyQuantity - 30;
+console.log(lilyQuantity1)
+
+roseValue1 = rose * roseQuantity1
+console.log(roseValue1)
+
+lilyVAlue1 = lily * lilyQuantity1
+console.log(lilyQuantity1)
+
+total = roseValue1 + lilyVAlue1 + tulipValue
+console.log(total)
+
+console.log("rose unit Prize :", rose , ",Quantity :", roseQuantity1 ,",value: ", roseValue1)
+console.log("rose unit Prize :", lily , ",Quantity :", lilyQuantity1 ,",value: ", lilyVAlue1)
+console.log("rose unit Prize :", tulip , ",Quantity :", tulipQuantity ,",value: ", tulipValue)
+console.log("total value :", total)
