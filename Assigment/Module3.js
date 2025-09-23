e5d02ccbf6994455fbc36a2eeecd34227c04ea41
Anj8494 <sharmaanjali3449@gmail.com>
2025-09-23 22:47:34 +0530

    module2 practice question ticket

--- Assigment/Module3.js
+++ Assigment/Module3.js
@@ -6,3 +6,10 @@ let ticket = {
 console.log("starting station:",ticket.from)
 console.log("end station:", ticket.to)
 console.log("price of  station:", ticket.price)
+
+let person = {
+    fristName : "Anjali",
+    lastName : "Sharma"
+}
+console.log("frist name:", person.fristName)
+console.log("last name:",person.lastName)
