5df5196703818a45624ddd401ccecde7b204ef99
Anj8494 <sharmaanjali3449@gmail.com>
2025-09-23 22:44:18 +0530

    module2 practice question ticket

---
+++ Assigment/Module3.js
@@ -0,0 +1,8 @@
+let ticket = {
+    from: "Indore",
+    to: "Ujjain",
+    price: "100"
+}
+console.log("starting station:",ticket.from)
+console.log("end station:", ticket.to)
+console.log("price of  station:", ticket.price)
