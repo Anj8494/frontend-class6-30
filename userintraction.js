d78535325a9c6b6a960d9cae4b15dd2ee5ac535c
Anj8494 <sharmaanjali3449@gmail.com>
2025-10-01 19:09:58 +0530

    if else examples

--- userintraction.js
+++ userintraction.js
@@ -76,17 +76,188 @@ console.log("this is my page")
     //     console.log("ngative")
     // }
 
-    let isUserReady  = confirm("Are you ready")
-        console.log(isUserReady)
-        if(isUserReady)
-        {
-            alert("User Ready")
-        }
-        else
-        {
-            alert("User not ready")
-        }
+    // let isUserReady  = confirm("Are you ready")
+    //     console.log(isUserReady)
+    //     if(isUserReady)
+    //     {
+    //         alert("User Ready")
+    //     }
+    //     else
+    //     {
+    //         alert("User not ready")
+    //     }
 
+//26/9/25 
+// if else details concepts
+// let age =prompt("Enter your age:")
+// if(age >= 18)
+// {
+//     alert("your are able to vote")
+// }
+// else 
+// {
+//     alert("your are a minor , not eligible to vote")
+// }
 
+//if elseif else
+// let age =prompt("Enter your age:")
+// if(age >= 18)
+// {
+//     alert("your are able to vote")
+// }
+// else if(age>=13)
+// {
+//     alert("your are a teenager, welcome to teenage")
+// }
+// else
+// {
+//     alert("your are a minor , not eligible to vote")
+// }
 
+//if else if else multipule
+// let number =prompt("Enter a Number:",0)
+// if(number <10)
+// {
+//     alert("<10")
+// }
+// else if(number <30)
+// {
+//     alert("<30")
+// }
+// else if(number < 60)
+// {
+//     alert("<60")
+// }
+// else if(number <80)
+// {
+//     alert("<80")
+// }
+// else
+// {
+//     alert(">80")
+// }
 
+// let age = prompt("enter your age")
+// let hasParentsAooroval =true;
+// let cartValue = 350
+// let points = 600
+// let shippinCost=50
+// let addInsurance= true
+// let InSURANCE_COST = 40
+// orderISValid = true
+// hasPromoCode = false
+// if(age< 21 && hasParentsAooroval === false)
+// {
+//     alert("oredr is vaild")
+// }
+// else if (age>21 && hasParentsAooroval === true)
+// {
+//     shippinCost = shippinCost -5
+//     alert("shippinCost :",shippinCost)
+// }
+// else if (age >=65)
+// {
+//     shippinCost=0
+//     alert(shippinCost)
+// }
+// else if(age >=21 && age <=64)
+// {
+//     if(hasParentsAooroval === true || cartValue >300  || points >500)
+//     {
+//         shippinCost =0
+//     }
+//     alert(shippinCost)
+// }
+// else (addInsurance === true && orderISValid === true && hasPromoCode === true) 
+// {
+//     shippinCost = shippinCost + InSURANCE_COST
+//     alert(shippinCost)
+// }
+
+// 30/9/25 
+//1. write a program to check number is even or odd
+// let num = prompt("enter the num")
+// if(num% 2===0)
+// {
+//     alert("num is even")
+// }
+// else 
+// {
+//     alert("num is odd")
+// }
+
+//2. write a program to get user name and age check check if user eligible to vote  or not if yes show an alert with respective message
+// let name = prompt("enter name")
+// let age = prompt("enter age")
+//  if (age>= 18)
+//  {
+//     alert(name + " your eligible for vote")
+//  }
+//  else
+//  {
+//     alert (name + "you are not eligible for vote")
+//  }
+
+//3.wirte a program to let user enter number of  products, let the price of produce be 12/- per product calculate the total price of all products
+// let numProduct = prompt("enter the number of product")
+// let peroductPrice = 12
+// let totalPrice = numProduct * peroductPrice
+// alert("total price:" + totalPrice)
+
+//write a program to ask for user's age and gender . show message "you are eligible for marrige accoring to old law",
+//"you are eligible for marrige according to new law"
+//(coorrect it its showing wrong) let gender = ("enter your gender")
+// let age = prompt("Enter your age")
+//  if ((gender == "male" && age >= 21) || (gender === "female" && age >=18))
+//  {
+//     alert ("your are eligible for merige according old law")
+//  }
+//  else if((gender == "male" && age >= 21) || (gender === "female" && age >=21))
+// {
+//     alert("you are not eligible for marrrige")
+//  }
+//  else{
+//     alert ("your are eligible for merige according new law")
+
+//  } 
+
+//let user enter the mark base on the marks enter give user the grad he scored
+//90-100 grade -a
+//80-90 -> b
+//70 -79-> c
+//60-69-> d
+//<60 ->fail
+// let score = prompt("enter your marks")
+// if(score>=90 && score <= 100)
+// {
+//     alert("grade A")
+// }
+// else if (score >= 80 && score<=89)
+// {
+//     alert("grade B")
+// }
+// else if (score >= 70 && score<=79)
+// {
+//     alert("grade C")
+// }
+// else if (score >= 60 && score<=69)
+//  {
+//         alert("grade D")
+//     }
+// else
+// {
+//     alert("you failed")
+
+// }
+
+//let user enter marks for 5 subject , calculate practentage of all marks and show hte % 
+let sub1= prompt("enter mark for sub 1")
+let sub2= prompt("enter mark for sub 2")
+let sub3= prompt("enter mark for sub 3")
+let sub4= prompt("enter mark for sub 4")
+let sub5= prompt("enter mark for sub 5")
+
+let totalMarks= sub1 + sub2 + sub3 + sub4 + sub5
+let maxMark= 5*100
+let percentage= (totalMarks/maxMark) *100
+alert("percentag :" ,percentage)
