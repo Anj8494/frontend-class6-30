bde9a14c19f2e7856929c8b11251e09321c397c4
Anj8494 <sharmaanjali3449@gmail.com>
2025-11-02 12:23:49 +0530

    operator 3  task

--- Assigment/Assigments.js
+++ Assigment/Assigments.js
@@ -15,8 +15,19 @@
 	// console.log(-17 _ 0); 
 	// console.log(25 _ 1); 
 	// console.log(2 + 2 * 2 _ 4);
-    console.log(4 * 5 == 20);
-	console.log(6 * 5 == "30");
-	console.log(-17 < 0); 
-	console.log(25 > 1); 
-	console.log(2 + 2 * 2 > 4);
+    // console.log(4 * 5 == 20);
+	// console.log(6 * 5 == "30");
+	// console.log(-17 < 0); 
+	// console.log(25 > 1); 
+	// console.log(2 + 2 * 2 > 4);
+
+//    3.  Fill in the missing comparison operators in that such a way that all expressions result in true (replace the underscore symbol with the appropriate operator):
+// 	console.log(true _ false); 
+// 	console.log(false _ false); 
+// 	console.log(false _ false _ true); 
+// 	console.log(true _ false _ false && true);
+
+console.log(true || false); 
+console.log(false == false); 
+console.log(false || false || true); 
+console.log(true || false || false && true);
