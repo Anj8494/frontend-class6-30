48407adea2bfe02997dfd5a0875d2d563343f515
Anj8494 <sharmaanjali3449@gmail.com>
2025-11-02 22:56:07 +0530

    module 4 practice questions

--- Assigment/Assigments.js
+++ Assigment/Assigments.js
@@ -85,13 +85,120 @@
 
 //module 4 practice question
 //1. Write a program that determines whether a given number is positive or not.
-let num = prompt("enter the number")
-if(num >0)
-{
-    alert("numberis positive")
-}
-else
-{
-    alert("number is nagative")
-}
+// let num = prompt("enter the number")
+// if(num >0)
+// {
+//     alert("numberis positive")
+// }
+// else
+// {
+//     alert("number is nagative")
+// }
 
+//2. Write a program that checks if a number is even or odd.
+// let num = prompt("enter the number")
+// if(num % 2 ==0)
+// {
+//     alert("number is even ")
+// }
+// else
+// {
+//     alert("number is odd")
+// }
+
+//3. rite a program to determine the greater of two numbers.
+// let num1 = prompt("enter the number")
+// let num2 = prompt("enter the number")
+// if(num1 > num2)
+// {
+//     alert( num1 + "number is greater then num 2 ")
+// }
+// else if( num2 > num1)
+// {
+//     alert(num2 + "is greater than num 1")
+// }
+// else
+// {
+//     alert("both number is equal")
+// }
+
+//4. Write a program that assigns a letter grade based on a numerical grade.
+// let mark = prompt("enter the number")
+// if(mark >=90)
+// {
+//     alert("Grade A")
+// }
+// else if(mark >=80)
+// {
+//     alert("Grade B")
+// }
+// else if(mark>= 70)
+// {
+//     alert("Grade C")
+// }
+// else
+// {
+//     alert("Grade D")
+// }
+
+// 5.Write a program that calculates the ticket price based on age.
+// let age = Number(prompt("Enter your age:"));
+// let ticketPrice;
+// if (Number.isNaN(age)) {
+//     console.log("Invalid input. Please enter a number.");
+// } else {
+//     if (age < 12) {
+//         ticketPrice = 50;   // ₹50 for kids
+//     } else if (age <= 60) {
+//         ticketPrice = 100;  // ₹100 for adults
+//     } else {
+//         ticketPrice = 70;   // ₹70 for senior citizens
+//     }
+
+//     console.log("Your ticket price is: ₹" + ticketPrice);
+//     alert("Your ticket price is: ₹" + ticketPrice);
+// }
+
+//6. Write a program that determines if a year is a leap year.
+// let year = prompt("enter a year")
+// if((year % 4 ===0 && year % 100 !== 0 ) || year % 400 === 0)
+// {
+//     alert("leap year")
+// }
+// else
+// {
+//     alert("not a leap year")
+// }
+
+//7. Write a program that calculates a discount based on the purchase amount.
+// let amount = prompt("enter the amount :")
+// let discount;
+// if(amount > 1000)
+// {
+//     discount = amount* 0.20
+// }
+// else if(amount > 500)
+// {
+//     discount = amount * 0.10
+// }
+// else
+// {
+//     discount = 0
+// }
+// alert("discount :"+ discount)
+// alert("final price:" + (amount - discount))
+
+// 8. Write a program that greets the user based on the time of day.
+let hour = prompt("enter the time")
+if (hour < 12)
+{
+    alert("Good morning")
+}
+else if(hour <18)
+{
+    alert("Good Afternoon")
+}
+else 
+{
+    alert("good evening")
+}
